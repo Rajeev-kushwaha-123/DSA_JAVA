277225f2dd26cc48e04d192cef2d5e3a507c7d9a
Rajeev-kushwaha-123 <rajeevkushwaha886@gmail.com>
2024-09-26 10:58:03 +0530

    basic java

---
+++ addTwoNumber.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class addTwoNumber {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter the first number:");
+
+        int a=sc.nextInt(); //storing the input inside the variable by using the nextInt
+
+        System.out.println("enter the second number:");
+
+        int b=sc.nextInt(); //storing the input inside the variable by using the nextInt
+
+        int sum=a+b;
+
+        System.out.println("The sum of two number is :" +sum);
+
+        sc.close();
+    }
+}
